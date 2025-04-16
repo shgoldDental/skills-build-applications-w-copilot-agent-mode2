06dd7aff4580c5d5d168067717c7680f0c72c730
Your Name <your.email@example.com>
2025-04-16 17:26:30 +0000

    Explicitly set app_label for models in octofit_tracker to resolve recognition issues

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -6,10 +6,16 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
+    class Meta:
+        app_label = 'octofit_tracker'
+
 class Team(models.Model):
     _id = models.ObjectIdField()
     name = models.CharField(max_length=100)
     members = models.ArrayReferenceField(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class Activity(models.Model):
     _id = models.ObjectIdField()
@@ -17,12 +23,21 @@ class Activity(models.Model):
     activity_type = models.CharField(max_length=100)
     duration = models.DurationField()
 
+    class Meta:
+        app_label = 'octofit_tracker'
+
 class Leaderboard(models.Model):
     _id = models.ObjectIdField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.IntegerField()
 
+    class Meta:
+        app_label = 'octofit_tracker'
+
 class Workout(models.Model):
     _id = models.ObjectIdField()
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    class Meta:
+        app_label = 'octofit_tracker'
